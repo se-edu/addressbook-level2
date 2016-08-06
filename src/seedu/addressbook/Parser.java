48bb518fb3c011df8b301b83448be072d7ec1529
Leow Yijin <leowyijin@gmail.com>
2016-08-07 00:13:41 +0800

    fix and finish parser

--- src/seedu/addressbook/Parser.java
+++ src/seedu/addressbook/Parser.java
@@ -21,9 +21,10 @@ public class Parser {
         ADD("add"),
         DELETE("delete"),
         CLEAR("clear"),
-        // read from address book
+        // updates view with multiple people
         FIND("find"),
         LIST("list"),
+        // read single person
         VIEW("view"),
         VIEW_ALL("viewall"),
         // misc
@@ -36,28 +37,6 @@ public class Parser {
         }
     }
 
-    public static void main(String... a) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        Parser p = new Parser();
-        while (true) {
-            try {
-                String full = sc.nextLine();
-                System.out.println();
-                System.out.println("\tCOMMAND: " + p.identifyCommand(full));
-                String args = p.extractArguments(full);
-                System.out.println("\tFULL_ARGS: " + args);
-                System.out.println("\tNAME: " + p.extractNameFromAddCommandArgs(args));
-                System.out.println("\tPHONE: " + (p.isPhonePrivateFromAddCommandArgs(args) ? "(private) " : "") + p.extractPhoneFromAddCommandArgs(args));
-                System.out.println("\tEMAIL: " + (p.isEmailPrivateFromAddCommandArgs(args) ? "(private) " : "") + p.extractEmailFromAddCommandArgs(args));
-                System.out.println("\tADDRESS: " + (p.isAddressPrivateFromAddCommandArgs(args) ? "(private) " : "") + p.extractAddressFromAddCommandArgs(args));
-                System.out.println("\tTAGS: " + Arrays.toString(p.extractTagsFromAddCommandArgs(args).toArray()));
-                System.out.println();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     public Parser() {}
 
     public static final String COMMENT_LINE_FORMAT_REGEX = "#.*";
@@ -65,7 +44,7 @@ public class Parser {
     public static final Pattern BASIC_COMMAND_FORMAT =
             Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
 
-    public static final Pattern ADD_COMMAND_ARGS_FORMAT = // '/' forward slashes are reserved
+    public static final Pattern ADD_COMMAND_ARGS_FORMAT = // '/' forward slashes are reserved for delimiter prefixes
             Pattern.compile(
                     "(?<name>[^/]+)"
                     + " (?<isPhonePrivate>p?)p/(?<phone>[^/]+)"
@@ -73,6 +52,13 @@ public class Parser {
                     + " (?<isAddressPrivate>p?)a/(?<address>[^/]+)"
                     + "(?<tagArguments>(?: t/[^/]+)*)" // variable number of tags
             );
+
+    // Same as VIEW and VIEW_ALL commands
+    public static final Pattern DELETE_COMMAND_ARGS_FORMAT =
+            Pattern.compile("(?<targetIndex>.+)");
+
+    public static final Pattern FIND_COMMAND_ARGS_FORMAT =
+            Pattern.compile("(?<keywords>\\S+(?:\\s+\\S+)*)"); // one or more keywords separated by whitespace
 
     /*
      * ===========================================
@@ -102,7 +88,7 @@ public class Parser {
     }
 
     /**
-     * Identifies the intended command type of the user input string.
+     * Identifies the intended command type of the user input string. Case-sensitive.
      *
      * @param rawInput full raw input from the user
      * @return the identity of the command
@@ -141,19 +127,19 @@ public class Parser {
 
     /*
      * ===========================================
-     *          ADD COMMAND ARG PARSING
+     *                ADD COMMAND
      * ===========================================
      */
 
     /**
      * Extracts the string representing the new person's name from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return the part of the arguments string representing the name
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public String extractNameFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public String extractNameFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -163,12 +149,12 @@ public class Parser {
     /**
      * Extracts the string representing the new person's phone from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return the part of the arguments string representing the phone
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public String extractPhoneFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public String extractPhoneFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -178,12 +164,12 @@ public class Parser {
     /**
      * Checks whether the new person's phone is private from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return true if the arguments string specifies that the person's phone should be marked private
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public boolean isPhonePrivateFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public boolean isPhonePrivateFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -193,12 +179,12 @@ public class Parser {
     /**
      * Extracts the string representing the new person's email from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return the part of the arguments string representing the email
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public String extractEmailFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public String extractEmailFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -208,12 +194,12 @@ public class Parser {
     /**
      * Checks whether the new person's email is private from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return true if the arguments string specifies that the person's email should be marked private
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public boolean isEmailPrivateFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public boolean isEmailPrivateFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -223,12 +209,12 @@ public class Parser {
     /**
      * Extracts the string representing the new person's address from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return the part of the arguments string representing the address
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public String extractAddressFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public String extractAddressFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -238,12 +224,12 @@ public class Parser {
     /**
      * Checks whether the new person's address is private from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return true if the arguments string specifies that the person's address should be marked private
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public boolean isAddressPrivateFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public boolean isAddressPrivateFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -253,12 +239,12 @@ public class Parser {
     /**
      * Extracts the strings representing the new person's tags from the arguments to the add command.
      *
-     * @param addArguments full add command arguments string as extracted from {@link #extractArguments(String)}
+     * @param addArgs full add command arguments string as extracted from {@link #extractArguments(String)}
      * @return list of all substrings representing tags from the arguments
      * @throws InvalidCommandFormatException if the arguments string is invalid
      */
-    public List<String> extractTagsFromAddCommandArgs(String addArguments) throws InvalidCommandFormatException {
-        Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArguments.trim());
+    public List<String> extractTagsFromAddCommandArgs(String addArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = ADD_COMMAND_ARGS_FORMAT.matcher(addArgs.trim());
         if (!matcher.matches()) {
             throw new InvalidCommandFormatException();
         }
@@ -267,4 +253,73 @@ public class Parser {
         // replace first delimiter prefix, then split
         return Arrays.asList(tagArguments.replaceFirst(" t/", "").split(" t/"));
     }
+    
+    /*
+     * ===========================================
+     *          SELECT PERSON COMMANDS
+     * ===========================================
+     */
+
+    /**
+     * Extracts the index number of the delete command's target from it's arguments.
+     *
+     * @param deleteArgs full delete command arguments string as extracted from {@link #extractArguments(String)}
+     * @return the index number of the target for deletion
+     * @throws InvalidCommandFormatException if the arguments string is invalid
+     */
+    public int extractIndexFromDeleteCommandArgs(String deleteArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = DELETE_COMMAND_ARGS_FORMAT.matcher(deleteArgs.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandFormatException();
+        }
+        try {
+            return Integer.parseInt(matcher.group("targetIndex"));
+        } catch (NumberFormatException nfe) {
+            throw new InvalidCommandFormatException();
+        }
+    }
+
+    /**
+     * Extracts the index number of the view command's target from it's arguments.
+     *
+     * @param viewArgs full view command arguments string as extracted from {@link #extractArguments(String)}
+     * @return the index number of the target for viewing
+     * @throws InvalidCommandFormatException if the arguments string is invalid
+     */
+    public int extractIndexFromViewCommandArgs(String viewArgs) throws InvalidCommandFormatException {
+        return extractIndexFromDeleteCommandArgs(viewArgs); // same args format as delete
+    }
+
+    /**
+     * Extracts the index number of the view all command's target from it's arguments.
+     *
+     * @param viewAllArgs full view all command arguments string as extracted from {@link #extractArguments(String)}
+     * @return the index number of the target for full viewing
+     * @throws InvalidCommandFormatException if the arguments string is invalid
+     */
+    public int extractIndexFromViewAllCommandArgs(String viewAllArgs) throws InvalidCommandFormatException {
+        return extractIndexFromDeleteCommandArgs(viewAllArgs); // same args format as delete
+    }
+
+    /*
+     * ===========================================
+     *              FIND COMMAND
+     * ===========================================
+     */
+
+    /**
+     * Extracts all keywords for the find command from it's arguments.
+     *
+     * @param findArgs full find command arguments string as extracted from {@link #extractArguments(String)}
+     * @return list of all search keywords specified in the arguments
+     * @throws InvalidCommandFormatException if the arguments string is invalid
+     */
+    public List<String> extractKeywordsFromFindCommandArgs(String findArgs) throws InvalidCommandFormatException {
+        final Matcher matcher = FIND_COMMAND_ARGS_FORMAT.matcher(findArgs.trim());
+        if (!matcher.matches()) {
+            throw new InvalidCommandFormatException();
+        }
+        return Arrays.asList(matcher.group("keywords").split("\\s+"));
+    }
+
 }
