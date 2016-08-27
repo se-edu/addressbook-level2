d5f37265d99a044ae1b47418a6fb0b9cbf45dc0b
amandachoy <amanda.choywy@gmail.com>
2016-08-27 18:24:25 +0800

    Update CommandResult.java

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    private final String feedbackToUser;
+    public final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
