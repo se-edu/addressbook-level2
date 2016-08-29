94571a24f616789dc22ef0c4c8d3aed86a06fcbc
hotchocolatemarshmallow <hotchocolatemarshmallow35211@gmail.com>
2016-08-29 23:40:32 +0800

    Made feedbackToUser private, added getter function getFeedbackToUser

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -25,6 +25,11 @@ public class CommandResult {
         this.feedbackToUser = feedbackToUser;
         this.relevantPersons = relevantPersons;
     }
+    
+    public String getFeedbackToUserpublic(){
+    	return feedbackToUser;
+    }
+
 
     /**
      * Returns list of persons relevant to the command command result, if any.
