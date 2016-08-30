090a65f9cd10d858f17caf97e7fbf90b7d14d14d
Jasper Lee <jasper.lee@u.nus.edu>
2016-08-30 22:25:41 +0800

    Split address to 4 classes

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+    private final String street;
+
+    public Street(String street) {
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+}
