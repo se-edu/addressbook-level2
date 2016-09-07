814c524caf32b3586acdf6fc7f2331c8327c8f4f
e0003315 <hujingrui@hotmail.com>
2016-09-08 04:32:25 +0800

    Create first test

--- src/seedu/addressbook/data/person/test/SimilarNameTest.java
+++ src/seedu/addressbook/data/person/test/SimilarNameTest.java
@@ -6,5 +6,13 @@ import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Name;
 
 public class SimilarNameTest {
+    
+    @Test
+    public void isSimilar_sameName_true() throws IllegalValueException {
+        Name n1 = new Name("ineedsleep");
+        Name n2 = new Name("ineedsleep");
+        assertTrue(n1.isSimilar(n2));
+    }
+
 
 }
