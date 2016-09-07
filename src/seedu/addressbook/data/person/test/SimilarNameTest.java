1b11759f44b32334502182374d7bf31816bd8a25
e0003315 <hujingrui@hotmail.com>
2016-09-08 04:27:08 +0800

    Create SimilarNameTest Class

---
+++ src/seedu/addressbook/data/person/test/SimilarNameTest.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.data.person.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.Name;
+
+public class SimilarNameTest {
+
+}
