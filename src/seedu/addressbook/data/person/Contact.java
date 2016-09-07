0a805f562b59ea70c84dab43ebf67a7bf3d191bc
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2016-09-07 20:08:21 +0800

    inheritance.Extract commonalities from Phone, Email, Address to Parent Class Contact.

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,13 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+	
+	public String value;
+	protected boolean isPrivate; 
+	
+	public Contact(String value, boolean isPrivate){
+		this.value=value;
+		this.isPrivate=isPrivate;
+	}
+
+}
