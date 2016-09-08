de9a43a2eac22911d041decc41d30df864bd8c7e
shaocloud <shao.cloud@gmail.com>
2016-09-09 00:10:02 +0800

    Add an interface, Printable, that is implemented by Name, Email, Address and Phone classes

--- src/seedu/addressbook/data/person/Email.java
+++ src/seedu/addressbook/data/person/Email.java
@@ -6,7 +6,7 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's email in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
  */
-public class Email {
+public class Email implements Printable {
 
     public static final String EXAMPLE = "valid@e.mail";
     public static final String MESSAGE_EMAIL_CONSTRAINTS =
@@ -54,6 +54,11 @@ public class Email {
         return value.hashCode();
     }
 
+
+    @Override
+    public String getPrintableString(){
+    	return "Email: " + this.toString();
+    }
 
     public boolean isPrivate() {
         return isPrivate;
