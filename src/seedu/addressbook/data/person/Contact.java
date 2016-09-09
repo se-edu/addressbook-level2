e6ba0d84b393d30c458261a103c3be8e16fbfc5d
Weng Kiat <wengkiat916@gmail.com>
2016-09-09 10:08:33 +0800

    created superclass contact

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+	
+	protected String value;
+	protected boolean isPrivate;
+	
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Phone // instanceof handles nulls
+                && this.value.equals(((Phone) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
+
+
