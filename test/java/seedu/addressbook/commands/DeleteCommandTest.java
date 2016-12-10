af722952e94de1a623edb5aff2ddde861c636041
Tan Wang Leng <tanwangleng@outlook.com>
2016-12-10 09:27:19 +0800

    Use more realistic test data

--- test/java/seedu/addressbook/commands/DeleteCommandTest.java
+++ test/java/seedu/addressbook/commands/DeleteCommandTest.java
@@ -34,11 +34,11 @@ public class DeleteCommandTest {
 
     @Before
     public void setUp() throws Exception {
-        Person johnDoe = new Person(new Name("John Doe"), new Phone("111", false),
+        Person johnDoe = new Person(new Name("John Doe"), new Phone("61234567", false),
                 new Email("john@doe.com", false), new Address("395C Ben Road", false), new UniqueTagList());
-        Person janeDoe = new Person(new Name("Jane Doe"), new Phone("222", false),
+        Person janeDoe = new Person(new Name("Jane Doe"), new Phone("91234567", false),
                 new Email("jane@doe.com", false), new Address("33G Ohm Road", false), new UniqueTagList());
-        Person davidGrant = new Person(new Name("David Grant"), new Phone("333", false),
+        Person davidGrant = new Person(new Name("David Grant"), new Phone("61121122", false),
                 new Email("david@grant.com", false), new Address("44H Define Road", false),
                 new UniqueTagList());
 
@@ -70,9 +70,9 @@ public class DeleteCommandTest {
     @Test
     public void execute_targetPersonNotInAddressBook_returnsPersonNotFoundMessage()
             throws IllegalValueException {
-        Person notInAddressBookPerson = new Person(new Name("Alien"), new Phone("10000", false),
-                new Email("a@b.c", false), new Address("Core of the Sun", false), new UniqueTagList());
-        List<ReadOnlyPerson> listWithPersonNotInAddressBook = Arrays.asList(notInAddressBookPerson);
+        Person notInAddressBookPerson = new Person(new Name("Not In Book"), new Phone("63331444", false),
+                new Email("notin@book.com", false), new Address("156D Grant Road", false), new UniqueTagList());
+        List<ReadOnlyPerson> listWithPersonNotInAddressBook = TestUtil.createList(notInAddressBookPerson);
 
         DeleteCommand command = createDeleteCommand(1, addressBook, listWithPersonNotInAddressBook);
 
