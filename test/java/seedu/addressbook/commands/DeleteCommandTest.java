3694772c46debedced3305d9eda40269626962f6
Tan Wang Leng <tanwangleng@outlook.com>
2016-12-10 10:22:14 +0800

    Remove unused imports

--- test/java/seedu/addressbook/commands/DeleteCommandTest.java
+++ test/java/seedu/addressbook/commands/DeleteCommandTest.java
@@ -2,8 +2,6 @@ package seedu.addressbook.commands;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
