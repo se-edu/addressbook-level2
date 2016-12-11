cb7db6900ba765dbfdf055c92c919cec151372cb
DesmondAng <monkeydesmond@gmail.com>
2016-12-11 18:34:06 +0800

    Remove TestPerson class

--- test/java/seedu/addressbook/util/TypicalTestPersons.java
+++ test/java/seedu/addressbook/util/TypicalTestPersons.java
@@ -10,23 +10,23 @@ import seedu.addressbook.data.person.UniquePersonList;
  */
 public class TypicalTestPersons {
 
-    public static TestPerson amy, bill, candy, donald, grace;
+    public static Person amy, bill, candy, donald, grace;
 
     public TypicalTestPersons() {
         try {
-            amy = new PersonBuilder().withName("Amy Buck").withPublicPhone("91234567")
-                    .withPublicEmail("cs@gmail.com").withPublicAddress("1 Clementi Road").build();
+            amy = new PersonBuilder().withName("Amy Buck").withPublicPhone("91119111")
+                    .withPublicEmail("ab@gmail.com").withPublicAddress("1 Clementi Road").build();
 
-            bill = new PersonBuilder().withName("Bill Clint").withPublicPhone("92434567")
-                    .withPublicEmail("bg@gmail.com").withPrivateAddress("2 Clementi Road").build();
+            bill = new PersonBuilder().withName("Bill Clint").withPublicPhone("92229222")
+                    .withPublicEmail("bc@gmail.com").withPrivateAddress("2 Clementi Road").build();
 
-            candy = new PersonBuilder().withName("Candy Destiny").withPrivatePhone("91934267")
-                    .withPublicEmail("sj@gmail.com").withPublicAddress("3 Clementi Road").build();
+            candy = new PersonBuilder().withName("Candy Destiny").withPrivatePhone("93339333")
+                    .withPublicEmail("cd@gmail.com").withPublicAddress("3 Clementi Road").build();
 
-            donald = new PersonBuilder().withName("Donald Eillot").withPrivatePhone("91214567")
-                    .withPrivateEmail("dk@gmail.com").withPublicAddress("4 Clementi Road").build();
+            donald = new PersonBuilder().withName("Donald Eillot").withPrivatePhone("94449444")
+                    .withPrivateEmail("de@gmail.com").withPublicAddress("4 Clementi Road").build();
 
-            grace = new PersonBuilder().withName("Grace Hopper").withPublicPhone("83434567")
+            grace = new PersonBuilder().withName("Grace Hopper").withPublicPhone("95559555")
                     .withPrivateEmail("gh@gmail.com").withPrivateAddress("5 Clementi Road").build();
 
         } catch (IllegalValueException e) {
@@ -37,7 +37,7 @@ public class TypicalTestPersons {
 
     public void loadAddressBookWithSampleData(AddressBook ab) {
         try {
-            for (TestPerson t : getTypicalPersons()) {
+            for (Person t : getTypicalPersons()) {
                 ab.addPerson(new Person(t));
             }
         } catch (UniquePersonList.DuplicatePersonException e) {
@@ -45,8 +45,8 @@ public class TypicalTestPersons {
         }
     }
 
-    public TestPerson[] getTypicalPersons() {
-        return new TestPerson[] {amy, bill, candy, donald, grace};
+    public Person[] getTypicalPersons() {
+        return new Person[] {amy, bill, candy, donald, grace};
     }
 
     public AddressBook getTypicalAddressBook() {
