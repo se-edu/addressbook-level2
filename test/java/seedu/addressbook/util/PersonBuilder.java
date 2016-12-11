cbabd32d5678d44a81c7c3991e32675e09a81aa5
DesmondAng <monkeydesmond@gmail.com>
2016-12-11 18:34:06 +0800

    Remove TestPerson class

--- test/java/seedu/addressbook/util/PersonBuilder.java
+++ test/java/seedu/addressbook/util/PersonBuilder.java
@@ -1,58 +1,68 @@
 package seedu.addressbook.util;
 
+import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Address;
 import seedu.addressbook.data.person.Email;
 import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.tag.UniqueTagList;
 
 /**
  * Factory class for building TestPerson
  */
 public class PersonBuilder {
 
-    private TestPerson person;
+    private Name name;
+    private Phone phone;
+    private Email email;
+    private Address address;
 
     public PersonBuilder() {
-        this.person = new TestPerson();
+        this.name = null;
+        this.phone = null;
+        this.email = null;
+        this.address = null;
     }
 
     public PersonBuilder withName(String name) throws IllegalValueException {
-        this.person.setName(new Name(name));
+        this.name = new Name(name);
         return this;
     }
     
     public PersonBuilder withPrivatePhone(String phone) throws IllegalValueException {
-        this.person.setPhone(new Phone(phone, true));
+        this.phone = new Phone(phone, true);
         return this;
     }
     
     public PersonBuilder withPublicPhone(String phone) throws IllegalValueException {
-        this.person.setPhone(new Phone(phone, false));
+        this.phone = new Phone(phone, false);
         return this;
     }
 
     public PersonBuilder withPrivateEmail(String email) throws IllegalValueException {
-        this.person.setEmail(new Email(email, true));
+        this.email = new Email(email, true);
         return this;
     }
 
     public PersonBuilder withPublicEmail(String email) throws IllegalValueException {
-        this.person.setEmail(new Email(email, false));
+        this.email = new Email(email, false);
         return this;
     }
     
     public PersonBuilder withPrivateAddress(String address) throws IllegalValueException {
-        this.person.setAddress(new Address(address, true));
+        this.address = new Address(address, true);
         return this;
     }
 
     public PersonBuilder withPublicAddress(String address) throws IllegalValueException {
-        this.person.setAddress(new Address(address, false));
+        this.address = new Address(address, false);
         return this;
     }
 
-    public TestPerson build() {
-        return this.person;
+    public Person build() {
+        assert !Utils.isAnyNull(this.name, this.phone, this.email, this.address);
+        return new Person(this.name, this.phone, this.email, this.address, new UniqueTagList());
     }
 }
