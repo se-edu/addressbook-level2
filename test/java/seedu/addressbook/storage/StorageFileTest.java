09fffb749d85998b8f2b898fe5893aec331f443f
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-12-22 22:47:36 +0800

    Change indentation and add comments

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -29,18 +29,21 @@ public class StorageFileTest {
 
     @Test
     public void initStorageFile_nullFilePath_exceptionThrown() throws InvalidStorageFilePathException {
+        //creating StroageFile with null filepath
         thrown.expect(NullPointerException.class);
         new StorageFile(null);
     }
 
     @Test
     public void initStorageFile_invalidFilePath_exceptionThrown() throws InvalidStorageFilePathException {
+        //creating StorageFile with file not ending with .txt
         thrown.expect(IllegalValueException.class);
         new StorageFile(TEST_DATA_FOLDER + "/InvalidFileName");
     }
 
     @Test
     public void load_invalidFormat_exceptionThrown() throws InvalidStorageFilePathException, StorageOperationException {
+        //the field in xml data does not match the AddressBook class, exception thrown 
         StorageFile storage = new StorageFile(TEST_DATA_FOLDER + "/InvalidData.txt");
         thrown.expect(StorageOperationException.class);
         storage.load();
@@ -50,17 +53,19 @@ public class StorageFileTest {
     public void load_validFormat() throws StorageOperationException, IllegalValueException {
         StorageFile storage = new StorageFile(TEST_DATA_FOLDER + "/ValidData.txt");
         AddressBook ab = storage.load();
-        Person testPerson = new Person(new Name("John Doe"), new Phone("98765432", false),
-                new Email("johnd@gmail.com", false), new Address("John street, block 123, #01-01", false),
-                new UniqueTagList(Collections.emptySet()));
+        Person testPerson = new Person(new Name("John Doe"), 
+                                       new Phone("98765432", false),
+                                       new Email("johnd@gmail.com", false), 
+                                       new Address("John street, block 123, #01-01", false),
+                                       new UniqueTagList(Collections.emptySet()));
         
         // ensure loaded AddressBook Object is properly constructed with sample data
         assertTrue(ab.getAllPersons().contains(testPerson));
     }
 
     @Test
-    public void save_nullAddressBook_exceptionThrown()
-            throws InvalidStorageFilePathException, StorageOperationException {
+    public void save_nullAddressBook_exceptionThrown() throws InvalidStorageFilePathException, StorageOperationException {
+        //save null AddressBook object to StorageFile
         StorageFile storage = new StorageFile(TEST_DATA_FOLDER + "/temp.txt");
         thrown.expect(NullPointerException.class);
         storage.save(null);
@@ -70,13 +75,15 @@ public class StorageFileTest {
     public void save_validAddressBook() throws IllegalValueException, StorageOperationException, IOException {
         StorageFile storage = new StorageFile(TEST_DATA_FOLDER + "/temp.txt");
         AddressBook ab = new AddressBook();
-        Person testPerson = new Person(new Name("John Doe"), new Phone("98765432", false),
-                new Email("johnd@gmail.com", false), new Address("John street, block 123, #01-01", false),
-                new UniqueTagList(Collections.emptySet()));
+        Person testPerson = new Person(new Name("John Doe"), 
+                                       new Phone("98765432", false),
+                                       new Email("johnd@gmail.com", false), 
+                                       new Address("John street, block 123, #01-01", false),
+                                       new UniqueTagList(Collections.emptySet()));
         ab.addPerson(testPerson);
         storage.save(ab);
 
-        // ensure xml data for sample data is properly saved
+        // ensure xml data for sample data is properly structured and saved
         assertTrue(compareFile("/temp.txt", "/ValidData.txt"));
     }
 
