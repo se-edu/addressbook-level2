533b598057f1f37a9a0123eab9798123418c6d7d
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-12-24 22:13:06 +0800

    Revert "Inline getStorage method"
    This reverts commit f431d12f12fd7bcdd30a512a85c3d1ed29f5ecfb.

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -18,6 +18,7 @@ import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.Phone;
 import seedu.addressbook.data.person.UniquePersonList;
 import seedu.addressbook.data.tag.UniqueTagList;
+import seedu.addressbook.storage.StorageFile.InvalidStorageFilePathException;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 
 public class StorageFileTest {
@@ -27,28 +28,28 @@ public class StorageFileTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void constructor_nullFilePath_exceptionThrown() throws Exception {
+    public void initStorageFile_nullFilePath_exceptionThrown() throws InvalidStorageFilePathException {
         thrown.expect(NullPointerException.class);
         new StorageFile(null);
     }
 
     @Test
-    public void constructor_noTxtExtension_exceptionThrown() throws Exception {
+    public void initStorageFile_noTxtExtension_exceptionThrown() throws InvalidStorageFilePathException {
         thrown.expect(IllegalValueException.class);
         new StorageFile(TEST_DATA_FOLDER + "/InvalidFileName");
     }
 
     @Test
-    public void load_invalidFormat_exceptionThrown() throws Exception {
+    public void load_invalidFormat_exceptionThrown() throws InvalidStorageFilePathException, StorageOperationException {
         // The file contains valid xml data, but does not match the AddressBook class
-        StorageFile storage = new StorageFile(TEST_DATA_FOLDER + "/InvalidData.txt");
+        StorageFile storage = getStorage("/InvalidData.txt");
         thrown.expect(StorageOperationException.class);
         storage.load();
     }
 
     @Test
-    public void load_validFormat() throws Exception {
-        AddressBook ab = new StorageFile(TEST_DATA_FOLDER + "/ValidData.txt").load();
+    public void load_validFormat() throws StorageOperationException, IllegalValueException {
+        AddressBook ab = getStorage("/ValidData.txt").load();
         Person testPerson = new Person(new Name("John Doe"), 
                                        new Phone("98765432", false),
                                        new Email("johnd@gmail.com", false), 
@@ -61,15 +62,15 @@ public class StorageFileTest {
     }
 
     @Test
-    public void save_nullAddressBook_exceptionThrown() throws Exception {
+    public void save_nullAddressBook_exceptionThrown() throws InvalidStorageFilePathException, StorageOperationException {
         // save null AddressBook object to StorageFile
         thrown.expect(NullPointerException.class);
-        new StorageFile(TEST_DATA_FOLDER + "/temp.txt").save(null);
+        getStorage("/temp.txt").save(null);
     }
 
     @Test
-    public void save_validAddressBook() throws Exception {
-        StorageFile storage = new StorageFile(TEST_DATA_FOLDER + "/temp.txt");
+    public void save_validAddressBook() throws IllegalValueException, StorageOperationException, IOException {
+        StorageFile storage = getStorage("/temp.txt");
         AddressBook ab = new AddressBook();
         Person testPerson = new Person(new Name("John Doe"), 
                                        new Phone("98765432", false),
@@ -83,7 +84,11 @@ public class StorageFileTest {
         compareFile("temp.txt", "ValidData.txt");
     }
 
-    private void compareFile(String file1, String file2) throws Exception {
+    private StorageFile getStorage(String filename) throws InvalidStorageFilePathException {
+        return new StorageFile(TEST_DATA_FOLDER + filename);
+    }
+
+    private void compareFile(String file1, String file2) throws IOException {
         String s1 = new String(Files.readAllBytes(Paths.get(TEST_DATA_FOLDER, file1)));
         String s2 = new String(Files.readAllBytes(Paths.get(TEST_DATA_FOLDER, file2)));
         assertTrue(s1.equals(s2));
