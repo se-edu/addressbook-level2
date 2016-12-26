ecd16b799d434ff38e3c86bced59e4a2f9cf072b
zzzzwen <zhiwen.song.loveslife@gmail.com>
2016-12-26 11:31:08 +0800

    remove unused import

--- test/java/seedu/addressbook/util/TestUtil.java
+++ test/java/seedu/addressbook/util/TestUtil.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
