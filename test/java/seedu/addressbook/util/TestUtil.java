24335db30633a4ba6c6e3339ad1736361b09bd8d
Song Zhiwen <zhiwen.song.loveslife@gmail.com>
2016-12-28 15:55:53 +0800

    [#104] Add unit tests for StorageFile class (#128)
    * Add unit test for StorageFile class
    
    * Change indentation and add comments
    
    * Make invalid data file more obvious
    
    * Create Utility method getStorage
    
    * Change contains method to equals method
    
    * Style change in StorageFileTest
    
    * Style changes in StorageFileTest
    
    * Inline getStorage method
    
    * Remove unused import
    
    * Revert "Inline getStorage method"
    
    This reverts commit f431d12f12fd7bcdd30a512a85c3d1ed29f5ecfb.
    
    * Re-adjust getStorage() method
    
    * Change assertTrue() to assertEquals
    
    * Move test files to correct folder
    
    * Add test persons and extract to a method
    
    * Rework assertSaveSuccess() method
    
    * Move compareFiles method to TestUtil class
    
    * Minor changes in StorageFile class
    
    * remove trailing whitespaces and unused imports
    
    * remove unused import
    
    * Change to return directly as addressbook
    
    * Rework and rename compareFile() to assertTextFilesEqual()
    
    * Use of temporary folder and minor style issues
    
    * Remove trailing whitespaces
    
    * minor style changes
    
    * Extract temp.txt to constant string
    
    * rework assertSaveSuccess() method
    
    * change of javadoc
    
    * Minor changes
    
    * Javadoc language
    
    * Javadoc change
    
    * Javadoc change

--- test/java/seedu/addressbook/util/TestUtil.java
+++ test/java/seedu/addressbook/util/TestUtil.java
@@ -1,7 +1,12 @@
 package seedu.addressbook.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,5 +64,15 @@ public class TestUtil {
             fail("test person data should be valid by definition");
             return null;
         }
+    }
+
+    /**
+     * Asserts whether the text in the two given files are the same. Ignores any
+     * differences in line endings
+     */
+    public static void assertTextFilesEqual(Path path1, Path path2) throws IOException {
+        List<String> list1 = Files.readAllLines(path1, Charset.defaultCharset());
+        List<String> list2 = Files.readAllLines(path2, Charset.defaultCharset());
+        assertEquals(String.join("\n", list1), String.join("\n", list2));
     }
 }
