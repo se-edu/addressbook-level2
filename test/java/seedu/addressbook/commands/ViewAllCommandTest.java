ea54ab772a04dc391d1f69c7ea3680e974ed30fa
zzzzwen <zhiwen.song.loveslife@gmail.com>
2017-01-04 11:15:54 +0800

    Grammar

--- test/java/seedu/addressbook/commands/ViewAllCommandTest.java
+++ test/java/seedu/addressbook/commands/ViewAllCommandTest.java
@@ -2,6 +2,6 @@ package seedu.addressbook.commands;
 
 public class ViewAllCommandTest {
     // ViewAll command is tested together with View command
-    // This is because they functions similarly but View command hides private information
+    // This is because they function similarly but View command hides private information
     // They are tested with same test data input
 }
