d94ecda25b992e317ff62dcbcf53e1aba1e6983c
Kelly <kelly.li.chang@gmail.com>
2017-01-31 21:56:46 +0800

    Encapsulate CommandResult class members

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,12 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
+    
+    /** Return feedback message */
+    public String showFeedbackToUser() {
+        return feedbackToUser;
+    }
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
