2bdc88cd38a40c823f7ec8190628cb313adf6a79
wanqingg <wanqing93@gmail.com>
2017-02-07 10:10:28 +0800

    Create Unit.java

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Unit {
+	
+	private final String unit;
+	
+	public Unit(String unit) {
+		this.unit = unit;
+	}
+	
+	public String getUnit() {
+		return this.unit;
+	}
+	
+}
