442ca37cced4bd14f951065078cbaff7f722b1aa
wanqingg <wanqing93@gmail.com>
2017-02-07 10:07:34 +0800

    Create Block.java

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Block {
+	
+	private final String block;
+	
+	public Block(String block) {
+		this.block = block;
+	}
+	
+	public String getBlock() {
+		return this.block;
+	}
+	
+}
