172f59f44680cfe4b81323fcabf4db851898d11a
wanqingg <wanqing93@gmail.com>
2017-02-07 10:09:04 +0800

    Create Street.java

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+	
+	private final String street;
+	
+	public Street(String street) {
+		this.street = street;
+	}
+	
+	public String getStreet() {
+		return this.street;
+	}
+	
+}
