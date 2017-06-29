e6abd1eec0d2f58a26ee4bbd0ae55726793341fd
luishengjie <luishengjie8@gmail.com>
2017-06-29 16:33:16 +0800

    Encapsulate CommandResult class members feedbackToUser - change to private

--- test/java/seedu/addressbook/commands/DeleteCommandTest.java
+++ test/java/seedu/addressbook/commands/DeleteCommandTest.java
@@ -109,7 +109,7 @@ public class DeleteCommandTest {
 
         CommandResult result = deleteCommand.execute();
 
-        assertEquals(expectedMessage, result.feedbackToUser);
+        assertEquals(expectedMessage, result.getFeedbackToUser());
         assertEquals(expectedAddressBook.getAllPersons(), actualAddressBook.getAllPersons());
     }
 
