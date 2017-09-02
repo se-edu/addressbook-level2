3554ec46700f12c222e57f8e827f3c3426b416a4
Jeffrey Goh <jeffreygohkw@gmail.com>
2017-09-03 01:12:51 +0800

    Encapsulate feedbackToUser in CommandResult

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -10,8 +10,7 @@ import java.util.Optional;
  */
 public class CommandResult {
 
-    /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -33,4 +32,8 @@ public class CommandResult {
         return Optional.ofNullable(relevantPersons);
     }
 
+    /** The feedback message to be shown to the user. Contains a description of the execution result */
+    public String getFeedbackToUser() {
+        return feedbackToUser;
+    }
 }
