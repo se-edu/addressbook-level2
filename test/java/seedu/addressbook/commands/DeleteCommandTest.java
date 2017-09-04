0525f7a5bad598a913ab4990272c7654a31c6870
Han Siang Phua <hansiang93@gmail.com>
2017-09-04 21:26:59 +0800

    Encapsulated CommandResult member String feedbackToUser

--- test/java/seedu/addressbook/commands/DeleteCommandTest.java
+++ test/java/seedu/addressbook/commands/DeleteCommandTest.java
@@ -109,7 +109,7 @@ public class DeleteCommandTest {
 
         CommandResult result = deleteCommand.execute();
 
-        assertEquals(expectedMessage, result.feedbackToUser);
+        assertEquals(expectedMessage, result.showFeedback());
         assertEquals(expectedAddressBook.getAllPersons(), actualAddressBook.getAllPersons());
     }
 
