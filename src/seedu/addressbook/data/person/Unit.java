55a698c7a38a65a25b468d6f970a1810b4286dc7
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2017-09-05 19:04:57 +0800

    Fix compilation error with Unit Class

--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -6,43 +6,43 @@ import seedu.addressbook.data.exception.IllegalValueException;
      * Represents a Person's address' unit in the address book.
      * Guarantees: immutable; is valid as declared in {@link #isValidUnit(String)}
      */
-    public class Unit {
+public class Unit {
 
-        public static final String EXAMPLE = "#12-34";
-        public static final String MESSAGE_UNIT_CONSTRAINTS =
-                "Person address' unit should start with #, followed by 2 numbers separated by '-'.";
-        public static final String UNIT_VALIDATION_REGEX = "#[\\d]+-[\\d]+";
+    public static final String EXAMPLE = "#12-34";
+    public static final String MESSAGE_UNIT_CONSTRAINTS =
+            "Person address' unit should start with #, followed by 2 numbers separated by '-'.";
+    public static final String UNIT_VALIDATION_REGEX = "#[\\d]+-[\\d]+";
 
-        public final String value;
+    public final String value;
 
 
-        /**
-         * Validates given address block.
-         *
-         * @throws IllegalValueException if given block string is invalid.
-         */
-        Unit(String unit) throws IllegalValueException {
-            if(!isValidUnit(unit)){
-                throw new IllegalValueException(MESSAGE_UNIT_CONSTRAINTS);
-            }
-
-            this.value = unit;
+    /**
+     * Validates given address unit.
+     *
+     * @throws IllegalValueException if given block string is invalid.
+     */
+    Unit(String unit) throws IllegalValueException {
+        if(!isValidUnit(unit)){
+            throw new IllegalValueException(MESSAGE_UNIT_CONSTRAINTS);
         }
 
-        public static boolean isValidUnit(String test) {
-            return test.matches(UNIT_VALIDATION_REGEX);
-        }
+        this.value = unit;
+    }
 
-        @Override
-        public String toString() {
-            return value;
-        }
+    public static boolean isValidUnit(String test) {
+        return test.matches(UNIT_VALIDATION_REGEX);
+    }
 
-        @Override
-        public int hashCode() {
-            return value.hashCode();
-        }
+    @Override
+    public String toString() {
+        return value;
+    }
 
+    @Override
+    public int hashCode() {
+        return value.hashCode();
     }
 
 }
+
+
