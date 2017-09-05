5340c42ffd1fbc541538100c7c04c5db459baca5
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2017-09-05 20:02:52 +0800

    Updated JUnit Tests

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -112,12 +112,12 @@ public class StorageFileTest {
         ab.addPerson(new Person(new Name("John Doe"),
                                 new Phone("98765432", false),
                                 new Email("johnd@gmail.com", false),
-                                new Address("John street, block 123, #01-01", false),
+                                new Address("123, John street, #123-01, 563221", false),
                                 new UniqueTagList(Collections.emptySet())));
         ab.addPerson(new Person(new Name("Betsy Crowe"),
                                 new Phone("1234567", true),
                                 new Email("betsycrowe@gmail.com", false),
-                                new Address("Newgate Prison", true),
+                                new Address("12, Newgate Prison, #12-01, 666666", true),
                                 new UniqueTagList(new Tag("friend"), new Tag("criminal"))));
         return ab;
     }
--- test/java/seedu/addressbook/util/TypicalPersons.java
+++ test/java/seedu/addressbook/util/TypicalPersons.java
@@ -20,13 +20,13 @@ public class TypicalPersons {
     public TypicalPersons() {
         try {
             amy = new Person(new Name("Amy Buck"), new Phone("91119111", false), new Email("ab@gmail.com", false),
-                    new Address("1 Clementi Road", false), new UniqueTagList());
+                    new Address("1, Clementi Road, #01-11, 1111", false), new UniqueTagList());
             bill = new Person(new Name("Bill Clint"), new Phone("92229222", false), new Email("bc@gmail.com", false),
-                    new Address("2 Clementi Road", true), new UniqueTagList());
+                    new Address("2, Clementi Road, #02-22, 2222", true), new UniqueTagList());
             candy = new Person(new Name("Candy Destiny"), new Phone("93339333", true),
-                    new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), new UniqueTagList());
+                    new Email("cd@gmail.com", false), new Address("3, Clementi Road, #03-33, 3333", true), new UniqueTagList());
             dan = new Person(new Name("Dan Smith"), new Phone("1234556", true), new Email("ss@tt.com", true),
-                    new Address("NUS", true), new UniqueTagList(new Tag("Test")));
+                    new Address("1, NUS, #01-01, 117417", true), new UniqueTagList(new Tag("Test")));
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
