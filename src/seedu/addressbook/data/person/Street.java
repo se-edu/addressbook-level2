0d6b7300ccaf31b7d5e2d0fffd00f0b09cfa1bf8
E0032186\JUSTIN TAN <e0032186@u.nus.edu>
2017-09-08 13:11:09 +0800

    w4.3d

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+public class Street {
+    private String streetName;
+    public Street(String streetName) {
+        this.streetName = streetName;
+    }
+    public String toString() {
+        return streetName;
+    }
+}
