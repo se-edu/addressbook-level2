266fd8fc2b66aa39d6e1cf19e48785e06b5a953f
shunyong <e0002695@u.nus.edu>
2017-09-11 21:58:46 +0800

    add method to call sort() in AddressBook.java

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -5,7 +5,7 @@ package seedu.addressbook.commands;
  * Lists all persons in the address book to the user.
  */
 
-public class SortCommand {
+public class SortCommand extends Command {
 
     public static final String COMMAND_WORD = "sort";
 
@@ -13,8 +13,14 @@ public class SortCommand {
             + ": Displays all persons in the address book as a list with index numbers in sorted order.\n"
             + "Example: " + COMMAND_WORD;
 
+    public static final String MESSAGE_SUCCESS = "List sorted successfully";
 
-    
 
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sort();
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 
 }
