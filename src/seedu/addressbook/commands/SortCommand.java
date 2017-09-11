17c8c606d1bf0697f8f012162bdbd10b9ff1ac3d
shunyong <e0002695@u.nus.edu>
2017-09-11 21:44:34 +0800

    added command word and message usage

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,4 +1,20 @@
 package seedu.addressbook.commands;
 
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+
 public class SortCommand {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book as a list with index numbers in sorted order.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    
+
+
 }
