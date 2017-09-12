5fa24d2c12b851f74f286e7ffcd989ee24cdcc06
marvinchin <chinkunsong.marvin@gmail.com>
2017-09-12 23:11:41 +0800

    Add AddTagsCommand

--- src/seedu/addressbook/commands/AddTagsCommand.java
+++ src/seedu/addressbook/commands/AddTagsCommand.java
@@ -1,27 +1,32 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList;
 import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.data.tag.UniqueTagList;
 
+import java.util.HashSet;
 import java.util.Set;
 
-public class AddTagCommand extends Command {
+public class AddTagsCommand extends Command {
     public static final String COMMAND_WORD = "addTags";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Adds the given tags to the person identified by the index number used in the last person listing.\n"
             + "Parameters: INDEX [t/TAG]...\n"
-            + "Example: " + COMMAND_WORD + "1 t/ friend t/ colleague";
+            + "Example: " + COMMAND_WORD + " 1 t/ friend t/ colleague";
     public static final String MESSAGE_ADD_TAGS_SUCCESS = "Tags added to person: %1$s";
 
     private final UniqueTagList tagList;
 
-    public AddTagCommand(int targetVisibleIndex, Set<Tag> tags) {
+    public AddTagsCommand(int targetVisibleIndex, Set<String> tags) throws IllegalValueException {
         super(targetVisibleIndex);
-
-        this.tagList = new UniqueTagList(tags);
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.tagList = new UniqueTagList(tagSet);
     }
 
     @Override
