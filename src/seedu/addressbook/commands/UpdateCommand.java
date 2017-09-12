3992b84b814ffd8719fe500848607632f42c3147
Lawrence Li <31027322+Zzmobie@users.noreply.github.com>
2017-09-12 21:46:39 +0800

    Implement UpdateCommand class

--- src/seedu/addressbook/commands/UpdateCommand.java
+++ src/seedu/addressbook/commands/UpdateCommand.java
@@ -1,8 +1,16 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
-import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.*;
 import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+import seedu.addressbook.data.tag.Tag;
+import seedu.addressbook.data.tag.UniqueTagList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static seedu.addressbook.commands.AddCommand.MESSAGE_DUPLICATE_PERSON;
 
 
 /**
@@ -18,26 +26,49 @@ public class UpdateCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1"
             + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
 
+
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
 
+    private final Person toAdd;
 
-    public UpdateCommand(int targetVisibleIndex) {
+    public UpdateCommand(int targetVisibleIndex, String name,
+                      String phone, boolean isPhonePrivate,
+                      String email, boolean isEmailPrivate,
+                      String address, boolean isAddressPrivate,
+                      Set<String> tags) throws IllegalValueException {
         super(targetVisibleIndex);
+        final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+        this.toAdd = new Person(
+                new Name(name),
+                new Phone(phone, isPhonePrivate),
+                new Email(email, isEmailPrivate),
+                new Address(address, isAddressPrivate),
+                new UniqueTagList(tagSet)
+        );
     }
-
 
     @Override
     public CommandResult execute() {
         try {
             final ReadOnlyPerson target = getTargetPerson();
             addressBook.removePerson(target);
-            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target));
-
+            try {
+                addressBook.addPerson(toAdd);
+            } catch (UniquePersonList.DuplicatePersonException dpe) {
+                return new CommandResult(MESSAGE_DUPLICATE_PERSON);
+            }
+            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target)
+                                                + String.format(MESSAGE_SUCCESS, toAdd));
         } catch (IndexOutOfBoundsException ie) {
             return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         } catch (PersonNotFoundException pnfe) {
             return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
         }
+
     }
 
 }
