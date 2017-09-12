87aefc6ac720d510440e7d9f2f56efe423cb24b8
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-09-12 22:36:38 +0800

    edit Contact.java to fix bugs on the previous version

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -3,9 +3,6 @@ package seedu.addressbook.data.person;
 import seedu.addressbook.data.exception.IllegalValueException;
 
 public class Contact {
-    public static String MESSAGE_CONTACT_CONSTRAINTS;
-    public static String CONTACT_VALIDATION_REGEX;
-
     public final String value;
     private boolean isPrivate;
 
@@ -16,18 +13,7 @@ public class Contact {
      */
     public Contact(String contact, boolean isPrivate) throws IllegalValueException {
         this.isPrivate = isPrivate;
-        String trimmedContact = contact.trim();
-        if (!isValidContact(trimmedContact)) {
-            throw new IllegalValueException(MESSAGE_CONTACT_CONSTRAINTS);
-        }
-        this.value = trimmedContact;
-    }
-
-    /**
-     * Returns true if the given string is a valid contact.
-     */
-    public boolean isValidContact(String test) {
-        return test.matches(CONTACT_VALIDATION_REGEX);
+        this.value = contact;
     }
 
     @Override
