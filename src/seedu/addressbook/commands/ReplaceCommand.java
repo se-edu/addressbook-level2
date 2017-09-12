b4d336c07041b5c54edd3262bdd5be72a2c1d7aa
Lawrence Li <31027322+Zzmobie@users.noreply.github.com>
2017-09-12 20:01:58 +0800

    Create ReplaceCommand class shell, updated Parser to call keyword from ReplaceCommand class

---
+++ src/seedu/addressbook/commands/ReplaceCommand.java
@@ -0,0 +1,42 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+
+
+/**
+ * Deletes a person identified using it's last displayed index from the address book.
+ */
+public class ReplaceCommand extends Command {
+
+    public static final String COMMAND_WORD = "replace";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the person identified by the index number used in the last person listing.\n"
+            + "Parameters: INDEX\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
+
+
+    public ReplaceCommand(int targetVisibleIndex) {
+        super(targetVisibleIndex);
+    }
+
+
+    @Override
+    public CommandResult execute() {
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            addressBook.removePerson(target);
+            return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, target));
+
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (PersonNotFoundException pnfe) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+        }
+    }
+
+}
