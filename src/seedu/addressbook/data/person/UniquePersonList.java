adcc3a1e1e3bef479c497eafce39168a3d452712
DESKTOP-D6JEPK4\user <lite0520@gmail.com>
2017-09-13 11:53:37 +0800

    changed the import * to individual import

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -1,6 +1,11 @@
 package seedu.addressbook.data.person;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;
