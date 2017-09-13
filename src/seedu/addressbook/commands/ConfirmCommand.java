9e4ded816afa8e951fbb961445b7a9ef2ed7b394
CuiXiaohan <cuixiaohan@me.com>
2017-09-13 14:47:24 +0800

    change the confirmation a bit less naive

--- src/seedu/addressbook/commands/ConfirmCommand.java
+++ src/seedu/addressbook/commands/ConfirmCommand.java
@@ -1,10 +1,10 @@
 package seedu.addressbook.commands;
 
 public class ConfirmCommand extends Command {
-    public static final String COMMAND_WORD = "Y";
+    public static final String COMMAND_WORD = "confirm";
 
     public static final String MESSAGE_WARN = "Your command may cause invertible harm to the data storage."
-            + "\nType 'Y' to confirm.";
+            + "\nType 'confirm' to confirm.";
 
     public ConfirmCommand() {}
 
