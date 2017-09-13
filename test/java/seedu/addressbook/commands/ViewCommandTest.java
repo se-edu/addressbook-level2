f060e18522a73d8bb2a8c5c467b2f4060153f9c6
deep4k <e0031591@u.nus.edu>
2017-09-13 23:01:51 +0800

    Change test files for [LO - ImplementClasses] , updated tests

--- test/java/seedu/addressbook/commands/ViewCommandTest.java
+++ test/java/seedu/addressbook/commands/ViewCommandTest.java
@@ -49,7 +49,7 @@ public class ViewCommandTest {
         ReadOnlyPerson stranger = new Person(new Name("me"),
                                              new Phone("123", true),
                                              new Email("some@hey.go", true),
-                                             new Address("nus", false),
+                                             new Address("111, nus, #11-11, 313131", false),
                                              new UniqueTagList(Collections.emptySet()));
         List<ReadOnlyPerson> listWithExtraPerson
                 = new ArrayList<ReadOnlyPerson>(listWithAllTypicalPersons);
--- test/java/seedu/addressbook/util/TypicalPersons.java
+++ test/java/seedu/addressbook/util/TypicalPersons.java
@@ -20,13 +20,14 @@ public class TypicalPersons {
     public TypicalPersons() {
         try {
             amy = new Person(new Name("Amy Buck"), new Phone("91119111", false), new Email("ab@gmail.com", false),
-                    new Address("1 Clementi Road", false), new UniqueTagList());
+                    new Address("123, 1 Clementi Road, #01-01, 010101", false), new UniqueTagList());
             bill = new Person(new Name("Bill Clint"), new Phone("92229222", false), new Email("bc@gmail.com", false),
-                    new Address("2 Clementi Road", true), new UniqueTagList());
+                    new Address("234, 2 Clementi Road, #02-02, 020202", true), new UniqueTagList());
             candy = new Person(new Name("Candy Destiny"), new Phone("93339333", true),
-                    new Email("cd@gmail.com", false), new Address("3 Clementi Road", true), new UniqueTagList());
+                    new Email("cd@gmail.com", false),
+                    new Address("345, 3 Clementi Road, #03-03, 030303", true), new UniqueTagList());
             dan = new Person(new Name("Dan Smith"), new Phone("1234556", true), new Email("ss@tt.com", true),
-                    new Address("NUS", true), new UniqueTagList(new Tag("Test")));
+                    new Address("456, NUS, #04-04, 040404", true), new UniqueTagList(new Tag("Test")));
         } catch (IllegalValueException e) {
             e.printStackTrace();
             assert false : "not possible";
