0d1e58f167c1c263aff1124751882ec5c095b81b
LAKESKY\Antz <emanasaur@gmail.com>
2017-09-13 23:16:18 +0800

    Add Unit class

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,17 @@
+package seedu.addressbook.data.person;
+
+public class Unit {
+    private int unitNum;
+    
+    public Unit(int unitNum) {
+        this.unitNum = unitNum;
+    }
+
+    public int getUnitNum() {
+        return unitNum;
+    }
+
+    public void setUnitNum(int unitNum) {
+        this.unitNum = unitNum;
+    }
+}
