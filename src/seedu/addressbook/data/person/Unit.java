04e791ea0691e29e2082ca3ce903800c94c91291
fustilio <lfrancis373@gmail.com>
2017-09-14 11:53:08 +0800

    Added UpdateCommand + added Address subclass

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Created by Francis on 14/9/2017.
+ */
+public class Unit {
+    protected final String Unit;
+
+    /**
+     * Validates given name.
+     */
+    public Unit(String unit) {
+        this.Unit = unit;
+    }
+}
---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,7 @@
+package seedu.addressbook.ui;
+
+/**
+ * Created by Francis on 14/9/2017.
+ */
+public class Formatter {
+}
