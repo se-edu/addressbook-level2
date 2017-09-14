d5cf556964965ce82fc707e07a7bbd8b7949bce1
Mechcalote <e00527972@u.nus.edu>
2017-09-14 16:32:22 +0800

    added SortCommand into commands

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,4 +1,15 @@
 package seedu.addressbook.commands;
 
-public class SortCommand {
+
+/**
+ * Sorts all the persons in the address book by alphabetical order
+ */
+
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts all persons in the address by alphabetical order. \n"
+            + "Example: " + COMMAND_WORD;
 }
