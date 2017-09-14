18f4e19f233242fdf93ef1ffa1a2915dd3d729d1
jiasheng <jiashengan@gmail.com>
2017-09-14 13:21:23 +0800

    1. editted to accomodate for password attribute. 2. Execute now also checks whether password is correct.

--- src/seedu/addressbook/commands/ViewAllCommand.java
+++ src/seedu/addressbook/commands/ViewAllCommand.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.Password.PasswordStorage;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
 
@@ -11,17 +12,20 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 public class ViewAllCommand extends Command {
 
     public static final String COMMAND_WORD = "viewall";
+    public String userInputPassword;
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Views the non-private details of the person "
             + "identified by the index number in the last shown person listing.\n"
             + "Parameters: INDEX\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Example: " + COMMAND_WORD + " 1 " + PasswordStorage.getExamplePassword();
 
     public static final String MESSAGE_VIEW_PERSON_DETAILS = "Viewing person: %1$s";
 
 
-    public ViewAllCommand(int targetVisibleIndex) {
+    public ViewAllCommand(int targetVisibleIndex, String userInputPassword) {
         super(targetVisibleIndex);
+        this.userInputPassword = userInputPassword;
+
     }
 
 
@@ -31,6 +35,9 @@ public class ViewAllCommand extends Command {
             final ReadOnlyPerson target = getTargetPerson();
             if (!addressBook.containsPerson(target)) {
                 return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+            }
+            if(!userInputPassword.equals(PasswordStorage.getActualPassword())){
+                return new CommandResult(Messages.MESSAGE_WRONG_PASSWORD);
             }
             return new CommandResult(String.format(MESSAGE_VIEW_PERSON_DETAILS, target.getAsTextShowAll()));
         } catch (IndexOutOfBoundsException ie) {
