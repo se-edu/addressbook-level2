18e7130c9ecb082b7889d9754cb2f016ba6c8c76
deep4k <e0031591@u.nus.edu>
2017-09-15 16:59:10 +0800

    change Parser.java to static and update ParserTest.java, add sequencing in Person.java

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -67,6 +67,7 @@ public class AddCommand extends Command {
     public CommandResult execute() {
         try {
             addressBook.addPerson(toAdd);
+            Person.updateNextSequenceNumber();
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (UniquePersonList.DuplicatePersonException dpe) {
             return new CommandResult(MESSAGE_DUPLICATE_PERSON);
