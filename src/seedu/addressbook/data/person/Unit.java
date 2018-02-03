53260ffbaded20643ea80720aa1ee637ad9649fb
yapni <yapni8@gmail.com>
2018-02-03 21:31:17 +0800

    Add new classes: Block, PostalCode, Street and Unit

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a unit in a Person's Address
+ */
+public class Unit {
+
+    private final String value;
+
+    public Unit(String unit) {
+        value = unit.trim();
+    }
+
+    public String getValue() { return value; }
+
+    @Override
+    public String toString() { return value; }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Unit // instanceOf handles null
+                && this.value.equals(((Unit) other).getValue())); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
