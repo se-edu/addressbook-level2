4e3f2203779509a6f9aef36f6382cdc8c235d5f6
yapni <yapni8@gmail.com>
2018-02-03 23:28:59 +0800

    Address.java: Split into more classes. Address now consists of Block, Street, Unit and PostalCode.

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -4,15 +4,22 @@ import seedu.addressbook.data.exception.IllegalValueException;
 
 /**
  * Represents a Person's address in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ * Guarantees: immutable
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS =
+            "Person addresses should be entered by block number, street, unit and followed by postal code"
+                    + "separated by ','";
+    public static final String SEPARATOR_COMMA = ", ";
+    // TODO: add ADDRESS_VALIDATION_REGEX for better input validation
 
     public final String value;
+    private final Block block;
+    private final Street street;
+    private final Unit unit;
+    private final PostalCode postalCode;
     private boolean isPrivate;
 
     /**
@@ -26,34 +33,60 @@ public class Address {
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+
+        String[] components = address.split(SEPARATOR_COMMA);
+        block = new Block(components[0]);
+        street = new Street(components[1]);
+        unit = new Unit(components[2]);
+        postalCode = new PostalCode((components[3]));
+        value = this.getFullAddressValue();
     }
 
     /**
      * Returns true if a given string is a valid person address.
      */
     public static boolean isValidAddress(String test) {
-        return test.matches(ADDRESS_VALIDATION_REGEX);
+        // TODO: A better validation for each component
+        return test.split(",").length == 4; // valid if contains block number, street, unit and postal code
+    }
+
+    public String getFullAddressValue() {
+        return block.toString() + SEPARATOR_COMMA + street.toString() + SEPARATOR_COMMA + unit.toString()
+                + SEPARATOR_COMMA + postalCode.toString();
+    }
+
+    public Block getBlock() { return block; }
+
+    public Street getStreet() { return street; }
+
+    public Unit getUnit() { return unit; }
+
+    public PostalCode getPostalCode() { return postalCode; }
+
+    public boolean isPrivate() {
+        return isPrivate;
     }
 
     @Override
     public String toString() {
-        return value;
+        return block.toString() + SEPARATOR_COMMA + street.toString() + SEPARATOR_COMMA + unit.toString()
+                + SEPARATOR_COMMA + postalCode.toString();
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.block.toString().equals(((Address) other).getBlock().toString())
+                && this.street.toString().equals(((Address) other).getStreet().toString()))
+                && this.unit.toString().equals((((Address) other).getUnit().toString()))
+                && this.postalCode.toString().equals(((Address) other).getPostalCode().toString()); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
-    }
-
-    public boolean isPrivate() {
-        return isPrivate;
+        String fullAddress = block.toString() + SEPARATOR_COMMA + street.toString() + SEPARATOR_COMMA + unit.toString()
+                + SEPARATOR_COMMA + postalCode.toString();
+        return fullAddress.hashCode();
     }
 }
