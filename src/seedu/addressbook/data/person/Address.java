08dfa5324dbc8d88328018f901afac4e588997d1
Song Weiyang <e0014576@u.nus.edu>
2018-02-07 22:15:50 +0800

    Address.java: add class, street.class

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -119,3 +119,19 @@ public class Unit{
     }
 }
 
+public class Street{
+    private String street;
+
+    public Street(String street){
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+}
+
