100a0fb97ddaf56eb3ffd8091bd1d261d3ad4c16
randypx <sinrandy@hotmail.com>
2018-02-08 00:22:42 +0800

    updated expected.txt and input.txt and a couple of minor display changes

--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -10,7 +10,7 @@ public class Unit {
 
     public static final String EXAMPLE = "#02-25";
     private static final String MESSAGE_UNIT_CONSTRAINTS =
-            "Unit numbers should start with '#'and be two numbers seperated by'-'";
+            "Unit numbers should start with '#'and be two numbers seperated by '-'";
     private static final String UNIT_VALIDATION_REGEX = "#\\d+-\\d+";
 
     public final String value;
