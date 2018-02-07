b4d06c4e119245c379a23d6b36b049f0d74c9baa
MSI\jas_p <jas_p90@hotmail.com>
2018-02-07 11:03:16 +0800

    LO-exception

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -106,12 +106,17 @@ public class Main {
      * @return result of the command
      */
     private CommandResult executeCommand(Command command)  {
+        command.setData(addressBook, lastShownList);
+        CommandResult result = command.execute();
         try {
             command.setData(addressBook, lastShownList);
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
-        } catch (Exception e) {
+        } catch  (StorageOperationException e) {
+            ui.showToUser(e.getMessage());
+            return result;
+        catch (Exception e) {
             ui.showToUser(e.getMessage());
             throw new RuntimeException(e);
         }
