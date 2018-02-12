6bd257ec775f4d649d26e11415260f6ba10a20e1
quentinkhoo <quentinkhoo@outlook.com>
2018-02-12 22:48:03 +0800

    Split TextUi class into Formatter class as well.

---
+++ src/seedu/addressbook/ui/Formatter.java
@@ -0,0 +1,129 @@
+package seedu.addressbook.ui;
+
+import static seedu.addressbook.common.Messages.MESSAGE_GOODBYE;
+import static seedu.addressbook.common.Messages.MESSAGE_INIT_FAILED;
+import static seedu.addressbook.common.Messages.MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE;
+import static seedu.addressbook.common.Messages.MESSAGE_USING_STORAGE_FILE;
+import static seedu.addressbook.common.Messages.MESSAGE_WELCOME;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import seedu.addressbook.commands.CommandResult;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+public class Formatter {
+    /** A decorative prefix added to the beginning of lines printed by AddressBook */
+    private static final String LINE_PREFIX = "|| ";
+
+    /** A platform independent line separator. */
+    private static final String LS = System.lineSeparator();
+
+    private static final String DIVIDER = "===================================================";
+
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+
+    /** Format of a comment input line. Comment lines are silently consumed when reading user input. */
+    private static final String COMMENT_LINE_FORMAT_REGEX = "#.*";
+
+    /**
+     * Returns true if the user input line should be ignored.
+     * Input should be ignored if it is parsed as a comment, is only whitespace, or is empty.
+     *
+     * @param rawInputLine full raw user input line.
+     * @return true if the entire user input line should be ignored.
+     */
+    public static boolean shouldIgnore(String rawInputLine) {
+        return rawInputLine.trim().isEmpty() || isCommentLine(rawInputLine);
+    }
+
+    /**
+     * Returns true if the user input line is a comment line.
+     *
+     * @param rawInputLine full raw user input line.
+     * @return true if input line is a comment.
+     */
+    public static boolean isCommentLine(String rawInputLine) {
+        return rawInputLine.trim().matches(COMMENT_LINE_FORMAT_REGEX);
+    }
+
+    public static String formatRequestUserCommandMessage() {
+        return LINE_PREFIX + "Enter command: ";
+    }
+
+    public static String formatDisplayRequestUserCommandMessage(String fullInputLine) {
+        return formatStringMessages("[Command entered:" + fullInputLine + "]");
+    }
+
+
+    public static String formatStringMessages(String... messages) {
+        StringBuilder sb = new StringBuilder();
+        for (String msg : messages) {
+            sb.append(LINE_PREFIX + msg.replace("\n", LS + LINE_PREFIX) + LS );
+        }
+        return sb.toString();
+    }
+
+    public static String formatWelcomeMessage(String version, String storageFilePath) {
+        String storageFileInfo = String.format(MESSAGE_USING_STORAGE_FILE, storageFilePath);
+        return formatStringMessages(
+                DIVIDER,
+                DIVIDER,
+                MESSAGE_WELCOME,
+                version,
+                MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE,
+                storageFileInfo,
+                DIVIDER);
+
+    }
+
+    public static String formatGoodbyeMessage() {
+        return formatStringMessages(
+                MESSAGE_GOODBYE,
+                DIVIDER,
+                DIVIDER);
+    }
+
+    public static String formatInitFailedMessage() {
+        return formatStringMessages(
+                MESSAGE_INIT_FAILED,
+                DIVIDER,
+                DIVIDER);
+    }
+
+    public static String formatFeedbackMessageToUser(String feedback) {
+        return formatStringMessages(
+                feedback,
+                DIVIDER);
+    }
+
+    /** Formats a list of strings as a viewable indexed list. */
+    public static String formatIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatStringMessages(formatted.toString());
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
+}
