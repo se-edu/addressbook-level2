0360bcd5980b20dbdad173a9e56cf44170392be9
张瑞琪 <zhangruiqi@Riqis-MacBook.local>
2018-02-13 14:37:02 +0800

    add a new command-sort

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,21 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
