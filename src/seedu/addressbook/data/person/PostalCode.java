d80edba7512e2aa289099936e92f5a6641c5efb4
张瑞琪 <zhangruiqi@Riqis-MacBook.local>
2018-02-13 14:54:10 +0800

    Implemented 4 classes

--- src/seedu/addressbook/data/person/PostalCode.java
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -1,4 +1,56 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class PostalCode {
+
+    public static final String EXAMPLE = "231534";
+    public static final String MESSAGE_POSTALCODE_CONSTRAINTS = "Address unit can be in any format";
+    public static final String POSTALCODE_VALIDATION_REGEX = ".+";
+
+    public final String value;
+    private boolean isPrivate;
+    /**
+     * Validates given block.
+     *
+     * @throws IllegalValueException if given name string is invalid.
+     */
+    public PostalCode(String postalcode, boolean isPrivate) throws IllegalValueException {
+        String trimmedPostalCode = postalcode.trim();
+        if (!isValidPostalCode(trimmedPostalCode)) {
+            throw new IllegalValueException(MESSAGE_POSTALCODE_CONSTRAINTS);
+        }
+        this.value = trimmedPostalCode;
+    }
+
+    /**
+     * Returns true if the given string is a valid person name.
+     */
+    public static boolean isValidPostalCode(String test) {
+        return test.matches(POSTALCODE_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PostalCode // instanceof handles nulls
+                && this.value.equals(((Unit) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 }
--- src/seedu/addressbook/data/person/Unit.java
+++ src/seedu/addressbook/data/person/Unit.java
@@ -1,4 +1,55 @@
 package seedu.addressbook.data.person;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Unit {
+    public static final String EXAMPLE = "#12-34";
+    public static final String MESSAGE_UNIT_CONSTRAINTS = "Address unit can be in any format";
+    public static final String UNIT_VALIDATION_REGEX = ".+";
+
+    public final String value;
+    private boolean isPrivate;
+    /**
+     * Validates given block.
+     *
+     * @throws IllegalValueException if given name string is invalid.
+     */
+    public Unit(String unit, boolean isPrivate) throws IllegalValueException {
+        String trimmedUnit = unit.trim();
+        if (!isValidUnit(trimmedUnit)) {
+            throw new IllegalValueException(MESSAGE_UNIT_CONSTRAINTS);
+        }
+        this.value = trimmedUnit;
+    }
+
+    /**
+     * Returns true if the given string is a valid person name.
+     */
+    public static boolean isValidUnit(String test) {
+        return test.matches(UNIT_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Unit // instanceof handles nulls
+                && this.value.equals(((Unit) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
 }
