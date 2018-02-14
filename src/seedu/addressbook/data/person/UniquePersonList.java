57125c20967cb0f038b057e25391fa1119c7153f
Livian1107 <e0134124@u.nus.edu>
2018-02-15 05:05:17 +0800

    update the method, the addressbook will be sorted with name in alphabetical order

--- src/seedu/addressbook/data/person/UniquePersonList.java
+++ src/seedu/addressbook/data/person/UniquePersonList.java
@@ -1,11 +1,6 @@
 package seedu.addressbook.data.person;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import seedu.addressbook.common.Utils;
 import seedu.addressbook.data.exception.DuplicateDataException;
@@ -120,6 +115,20 @@ public class UniquePersonList implements Iterable<Person> {
         if (!personFoundAndDeleted) {
             throw new PersonNotFoundException();
         }
+    }
+
+    /**
+     * Sort all persons from the list.
+     *
+     */
+    public void sort(){
+        System.out.println(("john doe").compareTo("e "));
+        Collections.sort(internalList, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return (o1.getName().toString().toUpperCase()).compareTo(o2.getName().toString().toUpperCase());
+            }
+        });
     }
 
     /**
