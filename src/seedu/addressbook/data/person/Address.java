e7070911ca90a84c43bf7d12499463b0e629cc90
Yu Hong Herald <yuhongherald@gmail.com>
2018-02-14 23:29:53 +0800

    untested, added block, street, unit and postalcode to composition of address

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -2,17 +2,30 @@ package seedu.addressbook.data.person;
 
 import seedu.addressbook.data.exception.IllegalValueException;
 
+import java.util.List;
+
 /**
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
+    public static final String ADDRESS_SEPARATOR = ",";
+    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String ADDRESS_VALIDATION_REGEX = "/^([^" + ADDRESS_SEPARATOR + "]*" +
+                                                            ADDRESS_SEPARATOR + "){3}[^,]*$/";
+    public static final int BLOCK_INDEX = 0;
+    public static final int STREET_INDEX = 1;
+    public static final int UNIT_INDEX = 2;
+    public static final int POSTALCODE_INDEX = 3;
+
 
     public final String value;
+    public final Block block;
+    public final Street street;
+    public final Unit unit;
+    public final PostalCode postalCode;
     private boolean isPrivate;
 
     /**
@@ -27,6 +40,11 @@ public class Address {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
         this.value = trimmedAddress;
+        String[] addressComponents = trimmedAddress.split("");
+        block = new Block(addressComponents[BLOCK_INDEX]);
+        street = new Street(addressComponents[STREET_INDEX]);
+        unit = new Unit(addressComponents[UNIT_INDEX]);
+        postalCode = new PostalCode(addressComponents[POSTALCODE_INDEX]);
     }
 
     /**
@@ -38,7 +56,10 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return block.toString() + ADDRESS_SEPARATOR +
+                street.toString() + ADDRESS_SEPARATOR +
+                unit.toString() + ADDRESS_SEPARATOR +
+                postalCode.toString() + ADDRESS_SEPARATOR;
     }
 
     @Override
