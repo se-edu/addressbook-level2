3928ca7317f96d494cee59fb43fdb6a2ff4ac92a
张瑞琪 <zhangruiqi@Riqis-MacBook.local>
2018-02-16 18:15:27 +0800

    Implemented the real sort command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -2,20 +2,37 @@ package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ */
+
 public class SortCommand extends Command {
-
     public static final String COMMAND_WORD = "sort";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Displays all persons in the address book as a list with index numbers.\n"
+            + ": Displays all persons in the address book sorted by names.\n"
             + "Example: " + COMMAND_WORD;
+     @Override
+     public CommandResult execute() {
+         List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+         List<ReadOnlyPerson> sortedPersons = new ArrayList<ReadOnlyPerson>();
+         int i,j;
+         for(i=0;i<allPersons.size();i++){
+             sortedPersons.add(allPersons.get(i));
+             }
+         int n=allPersons.size();
+         for(i=0;i<n-1;i++){
+             for(j=0;j<n-i-1;j++){
+                 if (sortedPersons.get(j).getName().toString().toLowerCase().compareTo(sortedPersons.get(j+1).getName().toString().toLowerCase()) > 0) {
+                     ReadOnlyPerson temp = sortedPersons.get(j);
+                     sortedPersons.set(j, sortedPersons.get(j + 1));
+                     sortedPersons.set(j + 1, temp);
+                 }
+             }
+         }
 
-
-    @Override
-    public CommandResult execute() {
-        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
-        return new CommandResult(getMessageForPersonSortShownSummary(allPersons), allPersons);
-    }
+         return new CommandResult(getMessageForPersonSortShownSummary(sortedPersons), sortedPersons);
+     }
 }
