f3ede7e435015a15a3f8a945ae1d868addb027f3
David <daviddalmaso@gmail.com>
2018-02-16 12:01:34 +0800

    Accept sort command but ignore it

--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -83,7 +83,7 @@ public class TextUi {
         String fullInputLine = in.nextLine();
 
         // silently consume all ignored lines
-        while (shouldIgnore(fullInputLine)) {
+        while (shouldIgnore(fullInputLine) || fullInputLine.compareTo("sort") == 0) {
             fullInputLine = in.nextLine();
         }
 
