b6787e7a4ea979b6f467f9f1bdcc5a3642ff961f
“YAMIDARKJunAn” <E0002937@u.nus.edu>
2018-08-06 06:31:51 +0800

    AdaptedAddressBook: remove master tag list
    AdaptedAddressBook keeps a list of tags, which is used for
    storing the master tag list in the AddressBook object.
    
    Since AddressBook no longer has a master tag list, it is unnecessary
    to store the master tag list in AdaptedAddressBook.
    
    Let's remove the master tag list in AdaptedAddressBook.
    
    Similarly, it is now unnecessary for AddressBook to have a
    method for retrieving the master tag list.
    
    Let's remove AddressBook#getTagList().

--- test/java/seedu/addressbook/data/AddressBookTest.java
+++ test/java/seedu/addressbook/data/AddressBookTest.java
@@ -76,24 +76,6 @@ public class AddressBookTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
-    public void addPerson_emptyAddressBook() throws Exception {
-        emptyAddressBook.addPerson(bobChaplin);
-        emptyAddressBook.addPerson(charlieDouglas);
-
-        assertTrue(isTagObjectInAddressBookList(tagMathematician, emptyAddressBook));
-        assertTrue(isTagObjectInAddressBookList(tagScientist, emptyAddressBook));
-    }
-
-    @Test
-    public void addPerson_someTagsNotInTagList() throws Exception {
-        assertFalse(isTagObjectInAddressBookList(tagEconomist, defaultAddressBook));
-        assertFalse(isTagObjectInAddressBookList(tagPrizeWinner, defaultAddressBook));
-        defaultAddressBook.addPerson(davidElliot);
-        assertTrue(isTagObjectInAddressBookList(tagEconomist, defaultAddressBook));
-        assertTrue(isTagObjectInAddressBookList(tagPrizeWinner, defaultAddressBook));
-    }
-
-    @Test
     public void addPerson_personAlreadyInList_throwsDuplicatePersonException() throws Exception {
         thrown.expect(DuplicatePersonException.class);
         defaultAddressBook.addPerson(aliceBetsy);
@@ -137,11 +119,10 @@ public class AddressBookTest {
     }
 
     @Test
-    public void clear() throws Exception {
+    public void clear() {
         defaultAddressBook.clear();
 
         assertTrue(isEmpty(defaultAddressBook.getAllPersons()));
-        assertTrue(isEmpty(defaultAddressBook.getAllTags()));
     }
 
     @Test
@@ -150,24 +131,5 @@ public class AddressBookTest {
         UniquePersonList personsToCheck = new UniquePersonList(aliceBetsy, bobChaplin);
 
         assertTrue(isIdentical(allPersons, personsToCheck));
-    }
-
-    @Test
-    public void getAllTags() throws Exception {
-        UniqueTagList allTags = defaultAddressBook.getAllTags();
-
-        assertTrue(allTags.contains(tagMathematician));
-    }
-
-    /**
-     * Returns true if the given Tag object is found in the tag list of the given AddressBook.
-     */
-    private boolean isTagObjectInAddressBookList(Tag tagToCheck, AddressBook addressBook) {
-        for (Tag tag : addressBook.getAllTags()) {
-            if (tag == tagToCheck) {
-                return true;
-            }
-        }
-        return false;
     }
 }
