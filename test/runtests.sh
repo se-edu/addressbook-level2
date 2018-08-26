423c98cf9a38e2df73aa18f7358927b26bfdd97c
eugenepeh <e0012835@u.nus.edu>
2018-08-26 13:08:02 +0800

    Fix runtests scripts proceeding on compilation error
    Runtests scripts compile the program under test, before executing the
    tests. The tests always execute regardless of whether the compilation
    succeeds or not.
    
    If the program has compilation errors, the runtests scripts are
    actually testing an older version of the program without the errors.
    This results in misleading test results.
    
    Let's teach the scripts to terminate immediately when the program fails
    to compile.

--- test/runtests.sh
+++ test/runtests.sh
@@ -12,8 +12,12 @@ then
     rm actual.txt
 fi
 
-# compile the code into the bin folder
-javac -cp ../src -Xlint:none -d ../bin ../src/seedu/addressbook/Main.java
+# compile the code into the bin folder, terminates if error occurred
+if ! javac -cp ../src -Xlint:none -d ../bin ../src/seedu/addressbook/Main.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
 
 # run the program, feed commands from input.txt file and redirect the output to the actual.txt
 java -classpath ../bin seedu.addressbook.Main < input.txt > actual.txt
