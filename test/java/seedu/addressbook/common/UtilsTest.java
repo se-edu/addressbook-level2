5bd0d380c80f7370b7633ce5aa8ca7f3e3e246c1
Keen Lok <keenloklai@gmail.com>
2018-09-02 14:14:29 +0800

    added test for null

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -4,24 +4,23 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
 public class UtilsTest {
-
-
+    
     @Test
     public void elementsAreUnique() throws Exception {
+        
         // empty list
         assertAreUnique();
-
+        
         // only one object
         assertAreUnique((Object) null);
         assertAreUnique(1);
         assertAreUnique("");
         assertAreUnique("abc");
-
+        
         // all objects unique
         assertAreUnique("abc", "ab", "a");
         assertAreUnique(1, 2);
@@ -35,6 +34,17 @@ public class UtilsTest {
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
     }
+    
+    @Test
+    public void elementsContainNull()  {
+        assertNotNull();
+        assertNotNull("abc");
+        assertNotNull("");
+        assertNotNull("abc", "ab", "a");
+        
+        assertHasNull(null);
+        assertHasNull( null, null);
+    }
 
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
@@ -42,5 +52,13 @@ public class UtilsTest {
 
     private void assertNotUnique(Object... objects) {
         assertFalse(Utils.elementsAreUnique(Arrays.asList(objects)));
+    }
+    
+    private void assertNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
+    
+    private void assertHasNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
     }
 }
