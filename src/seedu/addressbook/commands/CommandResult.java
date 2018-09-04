865dcb778195c43182d93deb22507937a141e320
U-ANDREWT\andre <andrew.jinshen@gmail.com>
2018-09-04 19:11:58 +0800

    Encapsulate CommandResult

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
@@ -31,6 +31,10 @@ public class CommandResult {
      */
     public Optional<List<? extends ReadOnlyPerson>> getRelevantPersons() {
         return Optional.ofNullable(relevantPersons);
+    }
+
+    public String getFeedbackToUser() {
+        return feedbackToUser;
     }
 
 }
