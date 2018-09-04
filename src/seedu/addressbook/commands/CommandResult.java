df7a2217ff2fdf4301efd058f893063ee790db2a
yao-feng <yaofeng96@gmail.com>
2018-09-04 14:20:07 +0800

    Apply Encapsulation [LO-Encapsulation]

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,7 +11,11 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
+
+    public String getFeedbackToUser() {
+        return feedbackToUser;
+    }
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
