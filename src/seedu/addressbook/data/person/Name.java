49b44266bbe62989d17b1c493e966df10038b29b
Russell Ong <digivicer@hotmail.com>
2018-09-05 13:34:04 +0800

    Exercise: Add a method in TDD fashion

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -60,4 +60,17 @@ public class Name {
         return fullName.hashCode();
     }
 
+    /**
+     * Returns true if the other name is very similar to this name.
+     * Two names are considered similar if ...
+     */
+    public boolean isSimilar(Name other) {
+        if(other == null) {
+            return false;
+        }
+        if(other.fullName.toLowerCase() == this.fullName.toLowerCase()) {
+            return true;
+        }
+        return false;
+    }
 }
