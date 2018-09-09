8998c0820e22caf22ca3a9f4e7f8ad91b30f7aaf
DESKTOP-GJ61B8F\xiangyu <1141108909@qq.com>
2018-09-09 11:28:16 +0800

    add SwapCommand to instruction list

--- src/seedu/addressbook/commands/SwapCommand.java
+++ src/seedu/addressbook/commands/SwapCommand.java
@@ -1,4 +1,13 @@
 package seedu.addressbook.commands;
 
-public class SwapCommand {
+public class SwapCommand extends Command{
+    public static final String COMMAND_WORD = "swap";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Swap the positions of two persons identified by indices in address book.\n"
+            + "Parameters: INDEX, INDEX\n"
+            + "Example: " + COMMAND_WORD + " 1,2";
+
+    public static final String MESSAGE_VIEW_PERSON_DETAILS = "Swap successfully.";
+
 }
