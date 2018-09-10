e0e730f44c69b3660c945fdbe8aec5e8ffbc8b15
DESKTOP-0MRO58O\Ang <e0176842@u.nus.edu>
2018-09-10 17:53:51 +0800

    Remove unnecessary imports

--- test/java/seedu/addressbook/commands/CountCommandTest.java
+++ test/java/seedu/addressbook/commands/CountCommandTest.java
@@ -1,21 +1,11 @@
 package seedu.addressbook.commands;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import org.junit.Test;
 
-import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.ReadOnlyPerson;
-import seedu.addressbook.util.TypicalPersons;
 
 public class CountCommandTest {
 
