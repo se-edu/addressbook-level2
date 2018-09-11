4a945cadfe8e8b681bfbc7d2464cf33388dd0ad5
florafong97 <e0042072@u.nus.edu>
2018-09-11 22:41:30 +0800

    implement sort command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,4 +1,19 @@
 package seedu.addressbook.commands;
 
-public class SortCommand {
+/**
+ * Sorts list of persons in address book
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + "Sorts all persons in address book. \n"
+            + "Example: " + COMMAND_WORD;
+
+    @Override
+    public CommandResult execute() {
+        addressBook.sort();
+        return new CommandResult("Address book has been sorted");
+    }
 }
