340429b9127da6499a4a37fc1a84ecb950823e28
bannified <freedomzelda_dc@hotmail.com>
2018-09-11 12:16:57 +0800

    Implement SortCommand

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,6 +1,10 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class SortCommand extends Command {
 
@@ -11,24 +15,53 @@ public class SortCommand extends Command {
             + "Example: " + COMMAND_WORD
             + " name -1";
 
-    public static final String MESSAGE_SUCCESS = "List sorted by: $s in %s order";
+    public static final String MESSAGE_INVALID_PROPERTY = "Unable to sort by invalid %s.";
+
+    public static final String MESSAGE_SUCCESS = "List sorted by: %s in %s order";
 
     private String propertyName;
 
     private boolean ascending;
 
     public SortCommand(String propertyName, String ascending) {
-        this.propertyName = propertyName;
+        this.propertyName = propertyName.toLowerCase();
         this.ascending = isSortAscending(ascending);
     }
 
     @Override
     public CommandResult execute() {
         // TO-DO: Add implementation (in a try-catch if required)
-        return new CommandResult(String.format(MESSAGE_SUCCESS,
-                propertyName,
-                (ascending) ? "ascending" : "descending"));
+        List<ReadOnlyPerson> personsList = addressBook.getAllPersons().clonedListView();
+        int sign = (ascending) ? 1 : -1;
+        switch (propertyName) {
+            case "name":
+                personsList.sort((o1, o2) -> {
+                    if (o1.hasSameData(o2))
+                        return 0;
+                    return sign * o1.getName().fullName.compareTo(o2.getName().fullName);
+                });
+                break;
+            case "email":
+                personsList.sort((o1, o2) -> {
+                    if (o1.hasSameData(o2))
+                        return 0;
+                    return sign * o1.getEmail().value.compareTo(o2.getEmail().value);
+                });
+                break;
+            case "address":
+                personsList.sort((o1, o2) -> {
+                    if (o1.hasSameData(o2))
+                        return 0;
+                    return sign * o1.getAddress().value.compareTo(o2.getAddress().value);
+                });
+                break;
+            default:
+                return new CommandResult(String.format(MESSAGE_INVALID_PROPERTY, propertyName));
+        }
+
+        return new CommandResult(getMessageForPersonListShownSummary(personsList), personsList);
     }
+
 
     /**
      * Determines if orderString is ascending or not.
@@ -37,26 +70,10 @@ public class SortCommand extends Command {
      */
     private static boolean isSortAscending(String orderString) {
         boolean ascending = true;
-        if (isInteger(orderString)) {
-            int orderParam = Integer.parseInt(orderString);
-            ascending = (orderParam < 0) ? false : true;
-        }
+        int orderParam = Integer.parseInt(orderString);
+        ascending = (orderParam < 0) ? false : true;
         return ascending;
     }
 
-    //TO-DO: Move to Utils
-    /**
-     * Checks if a given string is a valid integer.
-     * @param strNum
-     * @return true if given string can be parsed as an integer.
-     */
-    public static boolean isInteger(String strNum) {
-        try {
-            Integer num = Integer.parseInt(strNum);
-        } catch (NumberFormatException | NullPointerException nfe) {
-            return false;
-        }
-        return true;
-    }
 
 }
