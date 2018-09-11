852cece0860cbaebb91661df8b486f94e3504bd2
florafong97 <e0042072@u.nus.edu>
2018-09-11 23:04:13 +0800

    debug: ignore case when comparing names

--- src/seedu/addressbook/data/person/PersonComparator.java
+++ src/seedu/addressbook/data/person/PersonComparator.java
@@ -6,6 +6,6 @@ class PersonComparator implements Comparator<Person>{
     @Override
     public int compare(Person p1, Person p2) {
         return p1.getName().toString()
-                .compareTo(p2.getName().toString());
+                .compareToIgnoreCase(p2.getName().toString());
     }
 }
