51d787840cd3e46a3cfe4062646ec99d7036efeb
bannified <freedomzelda_dc@hotmail.com>
2018-09-11 10:58:23 +0800

    Change SortCommand constructor signature.

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -17,9 +17,9 @@ public class SortCommand extends Command {
 
     private boolean ascending;
 
-    public SortCommand(String propertyName, int ascending) {
-        this.propertyName = propertyName.toLowerCase().trim();
-        this.ascending = (ascending > 0);
+    public SortCommand(String propertyName, String ascending) {
+        this.propertyName = propertyName;
+        this.ascending = isSortAscending(ascending);
     }
 
     @Override
@@ -28,6 +28,35 @@ public class SortCommand extends Command {
         return new CommandResult(String.format(MESSAGE_SUCCESS,
                 propertyName,
                 (ascending) ? "ascending" : "descending"));
+    }
+
+    /**
+     * Determines if orderString is ascending or not.
+     * @param orderString
+     * @return true if orderString is positive.
+     */
+    private static boolean isSortAscending(String orderString) {
+        boolean ascending = true;
+        if (isInteger(orderString)) {
+            int orderParam = Integer.parseInt(orderString);
+            ascending = (orderParam < 0) ? false : true;
+        }
+        return ascending;
+    }
+
+    //TO-DO: Move to Utils
+    /**
+     * Checks if a given string is a valid integer.
+     * @param strNum
+     * @return true if given string can be parsed as an integer.
+     */
+    public static boolean isInteger(String strNum) {
+        try {
+            Integer num = Integer.parseInt(strNum);
+        } catch (NumberFormatException | NullPointerException nfe) {
+            return false;
+        }
+        return true;
     }
 
 }
