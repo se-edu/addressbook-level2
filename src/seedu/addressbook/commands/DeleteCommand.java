6f2fac50592e4cf8cc5c7176825f1e9b3eae90bc
remo5000 <vigneshshankar@gmail.com>
2018-09-13 21:26:32 +0800

    Modify constructor

--- src/seedu/addressbook/commands/DeleteCommand.java
+++ src/seedu/addressbook/commands/DeleteCommand.java
@@ -4,6 +4,10 @@ import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 
 /**
  * Deletes people identified using their last displayed indices from the address book.
@@ -18,10 +22,14 @@ public class DeleteCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1 2 3";
 
     public static final String MESSAGE_DELETE_PEOPLE_SUCCESS = "Deleted Person(s): %1$s %2$s %3$s";
+    private final List<Integer> targetVisibleIndices;
 
 
-    public DeleteCommand(int targetVisibleIndex) {
-        super(targetVisibleIndex);
+    public DeleteCommand(Set<Integer> targetVisibleIndices) {
+        this.targetVisibleIndices = new ArrayList<>();
+        for (Integer targetIndex : targetVisibleIndices) {
+            this.targetVisibleIndices.add(targetIndex);
+        }
     }
 
 
