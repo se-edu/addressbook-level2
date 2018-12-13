ea06e7fc67b22f02167cc9dbd18b642b69434f6c
Si Jie <sijie123@gmail.com>
2018-12-13 17:13:10 +0800

    Interdiff between v2 and v3

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -35,26 +35,26 @@ public class UtilsTest {
         assertNotUnique(null, null);
         assertNotUnique(null, "a", "b", null);
     }
-    
+
     @Test
     public void isAnyNull() {
         // no items
         assertFalse(Utils.isAnyNull());
-        
-        // varargs array that is null
-        assertFalse(Utils.isAnyNull(null));
-        
-        // one item which is null
+
+        // varargs array that is null (i.e. no array)
+        assertFalse(Utils.isAnyNull((Object[]) null));
+
+        // one item which is null (i.e. an array with one null item)
         assertTrue(Utils.isAnyNull((Object) null));
-        
+
         // at least one item, none of which are null
         assertFalse(Utils.isAnyNull("A"));
         assertFalse(Utils.isAnyNull("A", ""));
         assertFalse(Utils.isAnyNull(1, 2, "C"));
-        
+
         // at least one item, some of which are null
         assertTrue(Utils.isAnyNull(null, 1));
-        assertTrue(Utils.isAnyNull("A", null, "C"));   
+        assertTrue(Utils.isAnyNull("A", null, "C"));
     }
 
     private void assertAreUnique(Object... objects) {
