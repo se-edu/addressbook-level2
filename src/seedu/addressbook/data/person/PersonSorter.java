eec0f2a1bfb0ee0250f730a0a2745be838f45282
simjiazhi <e0177123@u.nus.edu>
2019-02-04 10:15:01 +0800

    Added PersonSorter Comparator

---
+++ src/seedu/addressbook/data/person/PersonSorter.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.data.person;
+
+import java.util.Comparator;
+
+/**
+ *
+ * Comparator class will be supplied into Collections.sort
+ * it will sort all Person object in AddressBook by name in
+ * alphabetical order only
+ */
+
+public class PersonSorter implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().fullName.compareTo(p2.getName().fullName);
+    }
+}
