255b0491febf902cd133ed52d55325fcbb8de66c
Creastery <Creastery@users.noreply.github.com>
2019-02-11 06:04:54 +0800

    Rename `actual.txt` and `expected.txt` to all uppercase
    On Windows, when the case-sensitive attribute of the `test` directory is
    set to `enabled` and `runtests.bat` is run, FC will report the following
    error:
    
        FC: cannot open ACTUAL.TXT - No such file or folder
    
    This is because FC internally transforms filenames to uppercase before
    opening them. Even though `runtests.bat` runs FC with the filenames
    `actual.txt` and `expected.txt`, FC will try to open the files
    `ACTUAL.TXT` and `EXPECTED.TXT` respectively. Usually this is not a
    problem since file operations are case-insensitive by default on
    Windows. However, when the `test` directory has the case-sensitive
    attribute enabled, FC would fail to find those files, hence the error.
    
    This results in developers being confused since `actual.txt` and
    `expected.txt` exists in the `test` directory. In addition, they will
    have to resort to use other tools to compare the actual and expected
    output of the program during testing.
    
    Although it is unlikely that developers enable the case attribute
    on the `test` directory intentionally, developers may accidentally do so
    without realising. For instance, because directories created using
    Windows Subsystem for Linux (WSL) enables the case-sensitive attribute
    by default, when developers perform `git clone` on the repository under
    WSL, all subdirectories (including `test`) created will have the
    case-sensitive attribute set to `enabled` automatically.
    
    To allow `fc` to open files in case-sensitive directories on Windows
    successfully, let's rename `actual.txt` and `expected.txt` to
    `ACTUAL.TXT` and `EXPECTED.TXT` respectively and update the
    documentation.

--- test/runtests.bat
+++ test/runtests.bat
@@ -4,7 +4,7 @@ REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
 REM delete output from previous run
-del actual.txt
+del ACTUAL.TXT
 
 REM compile the code into the bin folder
 javac  -cp ..\src -Xlint:none -d ..\bin ..\src\seedu\addressbook\Main.java
@@ -14,8 +14,8 @@ IF ERRORLEVEL 1 (
 )
 REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
-java -classpath ..\bin seedu.addressbook.Main < input.txt > actual.txt
+REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ..\bin seedu.addressbook.Main < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
-FC actual.txt expected.txt
+FC ACTUAL.TXT EXPECTED.TXT
--- test/runtests.sh
+++ test/runtests.sh
@@ -7,9 +7,9 @@ then
 fi
 
 # delete output from previous run
-if [ -e "./actual.txt" ]
+if [ -e "./ACTUAL.TXT" ]
 then
-    rm actual.txt
+    rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
@@ -19,11 +19,11 @@ then
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the actual.txt
-java -classpath ../bin seedu.addressbook.Main < input.txt > actual.txt
+# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
+java -classpath ../bin seedu.addressbook.Main < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
-diff actual.txt expected.txt
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
