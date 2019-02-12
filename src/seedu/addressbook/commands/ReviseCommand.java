a2858a4c72bcd6d5fe2bd6c3b498d882cae07c56
wSemis <wdbceg@gmail.com>
2019-02-12 15:22:35 +0800

    Program now accept revise command as view command

---
+++ src/seedu/addressbook/commands/ReviseCommand.java
@@ -0,0 +1,41 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+
+/**
+ * Shows details of the person identified using the last displayed index.
+ * Private contact details are not shown.
+ */
+public class ReviseCommand extends Command {
+
+    public static final String COMMAND_WORD = "revise";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Views the non-private details of the person "
+            + "identified by the index number in the last shown person listing.\n"
+            + "Parameters: INDEX\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_VIEW_PERSON_DETAILS = "Viewing person: %1$s";
+
+
+    public ReviseCommand(int targetVisibleIndex) {
+        super(targetVisibleIndex);
+    }
+
+
+    @Override
+    public CommandResult execute() {
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            if (!addressBook.containsPerson(target)) {
+                return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+            }
+            return new CommandResult(String.format(MESSAGE_VIEW_PERSON_DETAILS, target.getAsTextHidePrivate()));
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+    }
+
+}
