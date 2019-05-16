74f4f6d4c4ca5a74d9c5f4e1bd4691bebde7ef4f
Jeffry Lum <22460123+j-lum@users.noreply.github.com>
2019-05-16 15:52:02 +0800

    Migrate to JUnit 5 from JUnit 4
    Change imports to JUnit 5 imports.
    
    Replace @Before with @BeforeEach, ExpectedException with assertThrows,
    TemporaryFolder with TempDir where applicable as the former are
    depreciated in JUnit 5.
    
    Add dependencies for JUnit 5 to build.gradle.txt.

--- test/java/seedu/addressbook/parser/ParserTest.java
+++ test/java/seedu/addressbook/parser/ParserTest.java
@@ -1,7 +1,7 @@
 package seedu.addressbook.parser;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.addressbook.common.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.addressbook.common.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
 
@@ -9,8 +9,9 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import seedu.addressbook.commands.AddCommand;
 import seedu.addressbook.commands.ClearCommand;
@@ -32,11 +33,12 @@ import seedu.addressbook.data.person.Phone;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.data.tag.Tag;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParserTest {
 
     private Parser parser;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         parser = new Parser();
     }
