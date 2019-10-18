d6bfdebbb8dd721a4f1a511705b99cc5404b6d8f
María <mlovatonv@gmail.com>
2019-10-18 12:02:49 -0500

    add tagging class

---
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -0,0 +1,11 @@
+package java.seedu.addressbook.data.tag;
+
+public class Tagging {
+    Tag tag; 
+    Person person;
+
+    Tagging (Tag tag, Person person) {
+        this.tag = tag;
+        this.person = person;
+    }
+}
